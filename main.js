8c7c9e379646ddb711ee2c7880d060716c3e380d
exori <confusingsivan@gmail.com>
2019-06-14 01:32:00 +0200

    add image result div

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
-function report() {
-    let region = document.querySelector("body"); // whole screen
+let region = document.querySelector("body"); // whole screen
     html2canvas(region, {
       onrendered: function(canvas) {
         let pngUrl = canvas.toDataURL(); // png in dataURL format
-        let img = document.querySelector(".screen");
-        img.src = pngUrl; 
+        let result = document.querySelector(".result");
+        result.innerHTML = pngUrl;
   
-        console.log(pngUrl);
+        //console.log(pngUrl);
         // here you can allow user to set bug-region
         // and send it with 'pngUrl' to server
       },
     });
-  }
