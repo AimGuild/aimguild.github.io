44e4c6f094a7e83deed8c644210bff18be18da2d
exori <confusingsivan@gmail.com>
2019-06-14 00:45:00 +0200

    add geturlvars

--- js/main.js
+++ js/main.js
@@ -21,5 +21,26 @@ $(document)
   $('.main.menu  .ui.dropdown').dropdown({
 	on: 'hover'
   });
-})
-;
+
+  var urlVars = getUrlVars();
+
+  var decodedString = atob(urlVars.d);
+  var json = JSON.parse(decodedString);
+  console.log(json[0]);
+});
+
+
+
+
+function getUrlVars()
+{
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
